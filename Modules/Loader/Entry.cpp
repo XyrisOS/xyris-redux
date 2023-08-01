250301e6480bff9353aa3aaa44d25941ead50fa6
Keeton Feavel <keeton@xyr.is>
2023-07-31 23:13:40 -0400

    Add limine code into loader
    Using the Limine barebones repository as a guide since its more a test
    of whether or not the compiled binary works at all.

--- Modules/Loader/Entry.cpp
+++ Modules/Loader/Entry.cpp
@@ -1,5 +1,37 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <limine.h>
+
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0,
+    .response = NULL
+};
+
+static void hcf(void);
 extern "C" void loader_main(void);
 
+static void hcf(void) {
+    asm ("cli");
+    for (;;) {
+        asm ("hlt");
+    }
+}
+
 extern "C" void loader_main(void) {
-    //
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (size_t i = 0; i < 100; i++) {
+        uint32_t* fb_ptr = reinterpret_cast<uint32_t*>(framebuffer->address);
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
+    hcf();
 }
