477eb87fc69904d0147e214273755295883b975d
Keeton Feavel <keeton@xyr.is>
2023-08-01 22:14:42 -0400

    Create bootable image

--- CMake/Modules/InitializeStandardFlags.cmake
+++ CMake/Modules/InitializeStandardFlags.cmake
@@ -1,7 +1,12 @@
 set(C_CXX_WARNINGS -Wall -Werror -Wextra -Wundef -Winline -Wshadow -Wformat=2 -Wcast-align -Wno-long-long -Wpointer-arith -Wwrite-strings -Wredundant-decls -Wdouble-promotion -Wno-unused-function -Wmissing-declarations)
 set(C_WARNINGS ${C_CXX_WARNINGS} -Wnested-externs -Wstrict-prototypes -Wmissing-prototypes)
 set(CXX_WARNINGS ${C_CXX_WARNINGS})
+
+set(C_CXX_FLAGS -fdiagnostics-color=always)
+set(C_FLAGS ${C_CXX_FLAGS} ${C_WARNINGS})
+set(CXX_FLAGS ${C_CXX_FLAGS} ${CXX_WARNINGS})
+
 add_compile_options(
-    "$<$<COMPILE_LANGUAGE:CXX>:${CXX_WARNINGS}>"
-    "$<$<COMPILE_LANGUAGE:C>:${C_WARNINGS}>"
+    "$<$<COMPILE_LANGUAGE:CXX>:${CXX_FLAGS}>"
+    "$<$<COMPILE_LANGUAGE:C>:${C_FLAGS}>"
 )
--- build.sh
+++ build.sh
@@ -1,31 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 PROJECT_DIR=$(dirname "$(readlink -f "$0")")
+DIR_TOOLS="${PROJECT_DIR}/Tools"
 DIR_BUILD="${PROJECT_DIR}/Build"
 DIR_INSTALL="${PROJECT_DIR}/Distribution"
 DIR_CMAKE="${PROJECT_DIR}/CMake"
 DIR_TOOLCHAINS="${DIR_CMAKE}/Toolchains"
 
-show_help() {
-    echo "TODO"
-}
+light_blue="\033[1;34m"
+light_red="\033[1;31m"
+yellow="\033[1;33m"
+no_color="\033[0m"
 
-check_requirements() {
-    set -- "cmake" "ninja" "nasm" "mold"
-    for program in "$@"; do
-        if [ ! "$(command -v "${program}")" ]; then
-            echo "Did not find '${program}' in \$PATH."
-            exit 1
-        fi
-    done
+do_image_stage=1
+
+show_help() {
+    echo -e "TODO"
 }
 
 clean_stage() {
+    echo -e "### ${light_blue}Cleaning${no_color}"
     if [ -e "${DIR_BUILD}" ]; then
         rm -r "${DIR_BUILD}"
     fi
 }
 
+build_stage_check_requirements() {
+    for program in "cmake" "ninja" "nasm" "mold"; do
+        if [ ! "$(command -v "${program}")" ]; then
+            echo -e "${light_red}Did not find '${program}' in \$PATH.${no_color}"
+            exit 1
+        fi
+    done
+}
+
 build_stage() {
+    build_stage_check_requirements
+    echo -e "### ${light_blue}Building${no_color}"
     if [ ! -e "${DIR_BUILD}" ]; then
         mkdir "${DIR_BUILD}"
     fi
@@ -42,6 +53,63 @@ build_stage() {
     cmake --install "${DIR_BUILD}"
 }
 
+image_stage_check_requirements() {
+    for program in "dd" "mformat" "mmd" "mcopy"; do
+        if [ ! "$(command -v "${program}")" ]; then
+            echo -e "${light_red}Did not find '${program}' in \$PATH.${no_color}"
+            exit 1
+        fi
+    done
+}
+
+image_stage() {
+    image_stage_check_requirements
+    echo -e "### ${light_blue}Creating image${no_color}"
+    if [ ! -e "${DIR_INSTALL}" ]; then
+        echo -e "${light_red}'${DIR_INSTALL}' does not exist!${no_color}"
+        exit
+    fi
+
+    local kernel_bin="${DIR_INSTALL}/Kernel"
+    local limine_cfg="${DIR_INSTALL}/limine.cfg"
+    local limime_efi="${DIR_TOOLS}/limine/BOOTX64.EFI"
+    if [ ! -e "${limime_efi}" ]; then
+        # TODO: Add submodule warning / error message function
+        echo -e "${light_red}'${limime_efi}' does not exist! (Update your submodules!)${no_color}"
+        exit
+    fi
+
+    local image="${DIR_INSTALL}/boot.img"
+    if [ -e "${image}" ]; then
+        echo -e "${yellow}Removing old '${image}'...${no_color}"
+        rm "${image:?}"
+    fi
+
+    # Darwin uses BSD style `dd` so we need to adjust some flags
+    local bs="1M"
+    if [ "$(uname)" = "Darwin" ]; then
+        bs="1m"
+    fi
+
+    # FIXME:
+    # Eventually we want to keep the kernel in a separate `boot` partition,
+    # but for now it's easiest to just make one partition since we can't
+    # have multiple while we're using `mtools`
+
+    # 100 Mb is the minimum recommended size by most UEFI compatible systems
+    # Linux and Windows tend to be around 512 Mb whereas macOS is 200 Mb.
+    # We have no need for all that space, so for now we break convention.
+    dd if=/dev/zero of="${image}" bs="${bs}" count=32
+    # 53 cylinders ~= 32 Mb
+    echo "Formatting ${image}..."
+    mformat -i "${image}" -t 53 ::
+    mmd -i "${image}" ::/EFI
+    mmd -i "${image}" ::/EFI/BOOT
+    mcopy -i "${image}" "${kernel_bin}" ::
+    mcopy -i "${image}" "${limime_efi}" ::/EFI/BOOT
+    mcopy -i "${image}" "${limine_cfg}" ::/EFI/BOOT
+}
+
 while :; do
     case $1 in
         -h|-\?|--help)
@@ -52,13 +120,19 @@ while :; do
             clean_stage
             exit
             ;;
+        -k|--kernel-only)
+            do_image_stage=0
+            ;;
         --)
             # End of all options.
             shift
             break
             ;;
         -?*)
-            echo "Unknown option (ignored): '$1'"
+            show_help
+            echo
+            echo -e "${yellow}Unknown option: '$1'${no_color}"
+            exit
             ;;
         *)
             break
@@ -67,5 +141,7 @@ while :; do
     shift
 done
 
-check_requirements
 build_stage
+if [ $do_image_stage = 1 ]; then
+    image_stage
+fi
