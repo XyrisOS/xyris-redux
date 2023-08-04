8e20e6ebab26c5d010bb5141ff6fffc74d29bcac
Keeton Feavel <keeton@xyr.is>
2023-08-03 23:55:23 -0400

    Redo GDT table
    A misunderstanding in the Limine documentation lead me to believe I
    needed to format the GDT in the same way, but that's only true if the
    deprecated console features is used, otherwise the kernel is free to
    (and should) load a different GDT.

--- Modules/Kernel/Arch/GDT.cpp
+++ Modules/Kernel/Arch/GDT.cpp
@@ -15,7 +15,7 @@
 namespace GDT
 {
 
-const size_t gdtMaxEntries = 7;
+const size_t gdtMaxEntries = 5;
 struct Entry gdt[gdtMaxEntries];
 struct GDTR gdtr;
 
@@ -31,10 +31,6 @@ static void Flush(void)
 
 void Initialize(void)
 {
-    // FIXME: Many of these segments are nearly identical.
-    //        Create common definitions that can be used and
-    //        tweaked a little to reduce duplicate code.
-
     // Null segment
 
     size_t index = 0;
@@ -60,13 +56,13 @@ void Initialize(void)
         .reservedHigh = 0,
     };
 
-    // 16-bit segments
+    // Kernel
 
-    const union Base realCodeBase = { .value = 0 };
-    const union Limit realCodeLimit = { .value = 0x0000FFFF };
+    const union Base kernelCodeBase = { .value = 0 };
+    const union Limit kernelCodeLimit = { .value = 0x00FFFFFF };
     gdt[index++] = {
-        .limitLow = realCodeLimit.section.low,
-        .baseLow = realCodeBase.section.low,
+        .limitLow = kernelCodeLimit.section.low,
+        .baseLow = kernelCodeBase.section.low,
         .accessed = 0,
         .rw = 1,
         .dc = 0,
@@ -74,127 +70,81 @@ void Initialize(void)
         .system = 1,
         .privilege = 0,
         .present = 1,
-        .limitHigh = realCodeLimit.section.high,
-        .reserved = 0,
-        .longMode = 0,
-        .size = 0,
-        .granulatity = 0,
-        .baseMid = realCodeBase.section.mid,
-        .baseHigh = realCodeBase.section.high,
-        .reservedHigh = 0,
-    };
-
-    const union Base realDataBase = { .value = 0 };
-    const union Limit realDataLimit = { .value = 0x0000FFFF };
-    gdt[index++] = {
-        .limitLow = realDataLimit.section.low,
-        .baseLow = realDataBase.section.low,
-        .accessed = 0,
-        .rw = 1,
-        .dc = 0,
-        .executable = 0,
-        .system = 1,
-        .privilege = 0,
-        .present = 1,
-        .limitHigh = realDataLimit.section.high,
-        .reserved = 0,
-        .longMode = 0,
-        .size = 0,
-        .granulatity = 0,
-        .baseMid = realDataBase.section.mid,
-        .baseHigh = realDataBase.section.high,
-        .reservedHigh = 0,
-    };
-
-    // 32-bit segments
-
-    const union Base protectedCodeBase = { .value = 0 };
-    const union Limit protectedCodeLimit = { .value = 0xFFFFFFFF };
-    gdt[index++] = {
-        .limitLow = protectedCodeLimit.section.low,
-        .baseLow = protectedCodeBase.section.low,
-        .accessed = 0,
-        .rw = 1,
-        .dc = 0,
-        .executable = 1,
-        .system = 1,
-        .privilege = 0,
-        .present = 1,
-        .limitHigh = protectedCodeLimit.section.high,
-        .reserved = 0,
-        .longMode = 0,
-        .size = 1,
-        .granulatity = 1,
-        .baseMid = protectedCodeBase.section.mid,
-        .baseHigh = protectedCodeBase.section.high,
-        .reservedHigh = 0,
-    };
-
-    const union Base protectedDataBase = { .value = 0 };
-    const union Limit protectedDataLimit = { .value = 0xFFFFFFFF };
-    gdt[index++] = {
-        .limitLow = protectedDataLimit.section.low,
-        .baseLow = protectedDataBase.section.low,
-        .accessed = 0,
-        .rw = 1,
-        .dc = 0,
-        .executable = 0,
-        .system = 1,
-        .privilege = 0,
-        .present = 1,
-        .limitHigh = protectedDataLimit.section.high,
-        .reserved = 0,
-        .longMode = 0,
-        .size = 1,
-        .granulatity = 1,
-        .baseMid = protectedDataBase.section.mid,
-        .baseHigh = protectedDataBase.section.high,
-        .reservedHigh = 0,
-    };
-
-    // 64-bit segments
-
-    const union Base longCodeBase = { .value = 0 };
-    const union Limit longCodeLimit = { .value = 0 };
-    gdt[index++] = {
-        .limitLow = longCodeLimit.section.low,
-        .baseLow = longCodeBase.section.low,
-        .accessed = 0,
-        .rw = 1,
-        .dc = 0,
-        .executable = 1,
-        .system = 1,
-        .privilege = 0,
-        .present = 1,
-        .limitHigh = longCodeLimit.section.high,
+        .limitHigh = kernelCodeLimit.section.high,
         .reserved = 0,
         .longMode = 1,
         .size = 0,
-        .granulatity = 0,
-        .baseMid = longCodeBase.section.mid,
-        .baseHigh = longCodeBase.section.high,
+        .granulatity = 1,
+        .baseMid = kernelCodeBase.section.mid,
+        .baseHigh = kernelCodeBase.section.high,
         .reservedHigh = 0,
     };
 
-    const union Base longDataBase = { .value = 0 };
-    const union Limit longDataLimit = { .value = 0 };
+    const union Base kernelDataBase = { .value = 0 };
+    const union Limit kernelDataLimit = { .value = 0x00FFFFFF };
     gdt[index++] = {
-        .limitLow = longDataLimit.section.low,
-        .baseLow = longDataBase.section.low,
+        .limitLow = kernelDataLimit.section.low,
+        .baseLow = kernelDataBase.section.low,
+        .accessed = 0,
+        .rw = 1,
+        .dc = 0,
+        .executable = 0,
+        .system = 1,
+        .privilege = 0,
+        .present = 1,
+        .limitHigh = kernelDataLimit.section.high,
+        .reserved = 0,
+        .longMode = 0,
+        .size = 1,
+        .granulatity = 1,
+        .baseMid = kernelDataBase.section.mid,
+        .baseHigh = kernelDataBase.section.high,
+        .reservedHigh = 0,
+    };
+
+    // Userspace
+
+    const union Base userspaceCodeBase = { .value = 0 };
+    const union Limit userspaceCodeLimit = { .value = 0x00FFFFFF };
+    gdt[index++] = {
+        .limitLow = userspaceCodeLimit.section.low,
+        .baseLow = userspaceCodeBase.section.low,
         .accessed = 0,
         .rw = 1,
         .dc = 0,
         .executable = 1,
         .system = 1,
-        .privilege = 0,
+        .privilege = 3,
         .present = 1,
-        .limitHigh = longDataLimit.section.high,
+        .limitHigh = userspaceCodeLimit.section.high,
+        .reserved = 0,
+        .longMode = 1,
+        .size = 0,
+        .granulatity = 1,
+        .baseMid = userspaceCodeBase.section.mid,
+        .baseHigh = userspaceCodeBase.section.high,
+        .reservedHigh = 0,
+    };
+
+    const union Base userspaceDataBase = { .value = 0 };
+    const union Limit userspaceDataLimit = { .value = 0xFFFF };
+    gdt[index++] = {
+        .limitLow = userspaceDataLimit.section.low,
+        .baseLow = userspaceDataBase.section.low,
+        .accessed = 0,
+        .rw = 1,
+        .dc = 0,
+        .executable = 0,
+        .system = 1,
+        .privilege = 3,
+        .present = 1,
+        .limitHigh = userspaceDataLimit.section.high,
         .reserved = 0,
         .longMode = 0,
-        .size = 0,
-        .granulatity = 0,
-        .baseMid = longDataBase.section.mid,
-        .baseHigh = longDataBase.section.high,
+        .size = 1,
+        .granulatity = 1,
+        .baseMid = userspaceDataBase.section.mid,
+        .baseHigh = userspaceDataBase.section.high,
         .reservedHigh = 0,
     };
 
