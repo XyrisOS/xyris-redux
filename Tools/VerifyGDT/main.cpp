85bd823c6f2f2ced2e9865766031168def3f0dad
Keeton Feavel <keeton@xyr.is>
2023-12-27 16:03:42 -0500

    Create tool for viewing and debugging GDT structure

--- Tools/VerifyGDT/main.cpp
+++ Tools/VerifyGDT/main.cpp
@@ -1,8 +1,28 @@
 #include <Arch/GDT.hpp>
-#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include "../HexDump.hpp"
 
 int main(int argc, char* argv[]) {
     (void) argc;
     (void) argv;
+
+    GDT::Initialize();
     return EXIT_SUCCESS;
+}
+
+namespace GDT {
+    extern "C" void FlushGDT(GDTR* pGDTR) {
+        std::cout << "Flushing GDTR! [" << pGDTR << "]" << std::endl
+            << "  Size: " << pGDTR->size << std::endl
+            << "  Addr: " << pGDTR->addr << std::endl;
+
+        GDT* pGDT = reinterpret_cast<GDT*>(pGDTR->addr);
+        std::cout << "Validating GDT [" << pGDT << "]" << std::endl;
+        for (size_t i = 0; i != 6; i++) {
+            Entry& entry = pGDT->entries[i];
+            std::cout << "  [" << i << "] ";
+            HexDump(std::cout, &entry, sizeof(Entry), false);
+        }
+    }
 }
