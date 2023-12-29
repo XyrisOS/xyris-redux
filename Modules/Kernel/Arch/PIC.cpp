1e3aaba2e7fcf0d0a5ee8f4d2e79f74025d244fc
Keeton Feavel <keeton@xyr.is>
2023-12-29 18:22:47 -0500

    Save and restore PIC masks

--- Modules/Kernel/Arch/PIC.cpp
+++ Modules/Kernel/Arch/PIC.cpp
@@ -53,8 +53,8 @@ void Initialize(void)
     // TODO: There's a lot of magic going on here that I don't fully understand.
 
     // Save masks for restoration after PIC initialization
-    // uint8_t mask1 = IO::ReadByte(PIC1_DATA);
-    // uint8_t mask2 = IO::ReadByte(PIC2_DATA);
+    uint8_t mask1 = IO::ReadByte(PIC1_DATA);
+    uint8_t mask2 = IO::ReadByte(PIC2_DATA);
 
     // Start initialization of PIC
     // (in cascade mode so that slave PICs are also initialized)
@@ -76,8 +76,8 @@ void Initialize(void)
     IO::WriteByte(PIC2_DATA, ICW4Commands::INTEL_8086_MODE);
 
     // Restore masks saved before initialization
-    // IO::WriteByte(PIC1_DATA, mask1);
-    // IO::WriteByte(PIC2_DATA, mask2);
+    IO::WriteByte(PIC1_DATA, mask1);
+    IO::WriteByte(PIC2_DATA, mask2);
 }
 
 void Finalize(void)
