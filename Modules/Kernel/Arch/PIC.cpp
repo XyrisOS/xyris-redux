84e72dabc8360b551bbba034c08fb489359dd1fa
Keeton Feavel <keeton@xyr.is>
2023-12-29 23:32:36 -0500

    Explain some previously magic numbers in PIC.cpp

--- Modules/Kernel/Arch/PIC.cpp
+++ Modules/Kernel/Arch/PIC.cpp
@@ -39,11 +39,17 @@ enum Addresses : uint8_t {
     PIC2            = 0xA0, // IO base address for slave PIC
 };
 
-enum Actions : uint8_t {
+enum Operations : uint8_t {
     PIC1_COMMAND    = PIC1,
     PIC1_DATA       = (PIC1 + 1),
     PIC2_COMMAND    = PIC2,
     PIC2_DATA       = (PIC2 + 1),
+};
+
+enum Actions : uint8_t {
+    END_INERRUPT    = 0x20,
+    PIC2_CASCADE    = 0x02,
+    PIC1_CASCADE    = 0x04,
 };
 
 // Functions
@@ -52,32 +58,19 @@ void Initialize(void)
 {
     // TODO: There's a lot of magic going on here that I don't fully understand.
 
-    // Save masks for restoration after PIC initialization
-    uint8_t mask1 = IO::ReadByte(PIC1_DATA);
-    uint8_t mask2 = IO::ReadByte(PIC2_DATA);
-
     // Start initialization of PIC
     // (in cascade mode so that slave PICs are also initialized)
     IO::WriteByte(PIC1_COMMAND, ICW1Commands::INITIALIZE | ICW1Commands::ICW4_PRESENT);
     IO::WriteByte(PIC2_COMMAND, ICW1Commands::INITIALIZE | ICW1Commands::ICW4_PRESENT);
 
-    // Write master and slave PIC vector offsets
-    // TODO: How are these offsets determined? Are they correct for a 64-bit environment?
-    IO::WriteByte(PIC1_DATA, 0x20);
-    IO::WriteByte(PIC2_DATA, 0x28);
-
     // Tell master that a slave PIC exists at IRQ2 (0000 0100)
-    IO::WriteByte(PIC1_DATA, 0x04);
+    IO::WriteByte(PIC1_DATA, PIC1_CASCADE);
     // Tell slave that it should use a cascade identity of value `2`
-    IO::WriteByte(PIC2_DATA, 0x02);
+    IO::WriteByte(PIC2_DATA, PIC2_CASCADE);
 
     // Update PICs to 8086 mode instead of 8080 mode
     IO::WriteByte(PIC1_DATA, ICW4Commands::INTEL_8086_MODE);
     IO::WriteByte(PIC2_DATA, ICW4Commands::INTEL_8086_MODE);
-
-    // Restore masks saved before initialization
-    IO::WriteByte(PIC1_DATA, mask1);
-    IO::WriteByte(PIC2_DATA, mask2);
 }
 
 void Finalize(void)
@@ -88,18 +81,16 @@ void Finalize(void)
 
 void EndOfInterrupt(uint64_t id)
 {
-    constexpr uint8_t eoi = 0x20;
-
     // Interrupts and exceptions are treated the same and as such we need to
     // account for all the exceptions (32 of them) and then offset by the
     // interrupts that require acknowledgement on the secondary PIC (which is
     // 8 since the 8259 handles 8 interrupts per controller), which results
     // in a value of 0x28 (40 decimal).
     if (id >= 0x28) {
-        IO::WriteByte(PIC2_COMMAND, eoi);
+        IO::WriteByte(PIC2_COMMAND, END_INERRUPT);
     }
 
-    IO::WriteByte(PIC1_COMMAND, eoi);
+    IO::WriteByte(PIC1_COMMAND, END_INERRUPT);
 }
 
 }
