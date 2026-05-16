2590d62b75fd3cd5731114d578a785e439cfad71
Keeton Feavel <keetonfeavel@gmail.com>
2026-05-16 00:26:35 -0400

    Prevent Swift from assuming an allocator exists

--- Modules/CMake/Modules/InitializeEmbeddedSwift.cmake
+++ Modules/CMake/Modules/InitializeEmbeddedSwift.cmake
@@ -4,6 +4,8 @@ set(CMAKE_Swift_COMPILATION_MODE wholemodule)
 set(SWIFT_EMBEDDED_FLAGS
     -parse-as-library
     -enable-experimental-feature Embedded
+    -no-allocations
+    -Osize
     -target ${XYRIS_SWIFT_TARGET_TRIPLE}
     -module-cache-path ${XYRIS_SWIFT_MODULE_CACHE_PATH}
 )
