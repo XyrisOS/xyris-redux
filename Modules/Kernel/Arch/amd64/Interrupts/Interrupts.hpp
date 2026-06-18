f7efae1cccd4d622507351a38d66f3a66f701578
Keeton Feavel <keeton@xyr.is>
2026-06-17 22:11:44 -0400

    Add FLAGS handling to improve interrupt safety

--- Modules/Kernel/Arch/amd64/Interrupts/Interrupts.hpp
+++ Modules/Kernel/Arch/amd64/Interrupts/Interrupts.hpp
@@ -10,6 +10,7 @@
  */
 
 #pragma once
+#include <CPU/FlagsR.hpp>
 
 namespace Interrupts
 {
@@ -17,11 +18,26 @@ namespace Interrupts
 void Enable();
 void Disable();
 
-template<typename T> void CriticalRegion(T work)
+template<typename T>
+void CriticalRegion(T work)
 {
-    Disable();
+    // Safely handle interrupts already being disabled during critical section
+    // by saving FLAGS register contents and checking if interrupts were enabled
+    // before re-enabling upon critical work exit.
+    Registers::FlagsR flags = { .value = 0 };
+    asm volatile(
+        "pushfq\n"
+        "pop %0\n"
+        "cli"
+        : "=r"(flags.value)
+        :
+        : "memory");
+
     work();
-    Enable();
+
+    if (flags.section.interruptEnable != 0) {
+        Enable();
+    }
 }
 
 }
